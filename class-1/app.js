fa01ab96b6022ceeb9a17af2db2aab1d0290b232
fatehullah778866 <fatehmsd2522004@gmail.com>
2025-09-03 00:19:48 +0500

    class-1 done

---
+++ class-1/app.js
@@ -0,0 +1,13 @@
+// string
+var myName = "John Doe";
+//Number
+var myAge = 30;
+// Boolean
+var isStudent = true;
+// Any (not recommended)
+var randomValue = 42;
+randomValue = "Now I'm a string";
+document.write("Name: " + myName + "<br>");
+document.write("Age: " + myAge + "<br>");
+document.write("Student: " + isStudent + "<br>");
+document.write("Random: " + randomValue + "<br>");
---
+++ class-1/app.ts
@@ -0,0 +1,20 @@
+// string
+
+let myName: string = "John Doe";
+
+//Number
+
+let myAge: number = 30;
+
+
+// Boolean
+let isStudent: boolean = true;
+
+// Any (not recommended)
+let randomValue: any = 42;
+randomValue = "Now I'm a string";
+
+document.write("Name: " + myName + "<br>");
+document.write("Age: " + myAge + "<br>");
+document.write("Student: " + isStudent + "<br>");
+document.write("Random: " + randomValue + "<br>");
