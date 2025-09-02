c02c23d1cd0cb52dcba8ba5526c65ae16c608c42
fatehullah778866 <fatehmsd2522004@gmail.com>
2025-09-03 00:30:45 +0500

    app.ts updated

--- class-2/app.js
+++ class-2/app.js
@@ -2,3 +2,8 @@ function greetUser(name) {
     return "hello, ".concat(name, "! Welcome to TypeScript");
 }
 document.write(greetUser("John Doe"));
+// adding two numbers
+function addNumbers(num1, num2) {
+    return num1 + num2;
+}
+document.write("Sum: " + addNumbers(5, 10));
--- class-2/app.ts
+++ class-2/app.ts
@@ -3,3 +3,13 @@ function greetUser(name:string):string {
 }
 
 document.write(greetUser("John Doe"));
+
+
+
+// adding two numbers
+
+function addNumbers(num1:number, num2:number):number{
+    return num1 + num2;
+}
+
+document.write("Sum: " + addNumbers(5, 10));
